d35976d86bbca554c10107fc045f3d38ba6f1d07
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-05-25 09:14:45 -0700

    added scripts

---
+++ blocksgame/Assets/followCam.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class followCam : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offest;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offest;
+    }
+}
---
+++ blocksgame/Assets/playermovment.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class playermovment : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 1000f;
+    //for pysics
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, speed * Time.deltaTime);
+    }
+}
